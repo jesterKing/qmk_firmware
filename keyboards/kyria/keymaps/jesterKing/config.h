b36dc2aabc0202289c2dc389954d210a841978ad
Nathan Letwory <nathan@mcneel.com>
2021-07-30 17:25:58 +0300

    Add jesterKing keymap.
    * QWERTY layout on layer 0
    * DAS layout on layer 1
    * STENO layout on layer 2, GeminiPR
    
    * Layers 0 and 1 are base layers
    
    * Only rainbow swirl animation, adapted led map

---
+++ keyboards/kyria/keymaps/jesterKing/config.h
@@ -0,0 +1,47 @@
+/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define EE_HANDS
+
+// The Leader key allows to flexibly assign macros to key sequences.
+#define LEADER_PER_KEY_TIMING
+#define LEADER_NO_TIMEOUT
+#define LEADER_TIMEOUT 350
+
+#define TAPPING_TERM 200
+
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X64
+  #define OLED_TIMEOUT 5000
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+  //#define RGBLIGHT_ANIMATIONS enabling all animations uses 2900 more bytes
+  //#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+  /*#define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_LIMIT_VAL 250*/
+  #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13}
+#endif
+
+#define ENCODER_DIRECTION_FLIP
+
+#define RGBLIGHT_DISABLE_KEYCODES // saves 480 bytes
+
+#define UNICODE_SELECTED_MODES UC_WINC, UC_MAC, UC_LNX
--- keyboards/kyria/rev1/config.h
+++ keyboards/kyria/rev1/config.h
@@ -19,11 +19,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0xFAB0
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    splitkb
-#define PRODUCT         Kyria Keyboard
+#define MANUFACTURER    jesterKing
+#define PRODUCT         jesterKing Keyboard
 
 /* key matrix size */
 /* Rows are doubled up */
