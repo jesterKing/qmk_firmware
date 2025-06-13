a7e4ac89fe32691281e128696c1172172be27d51
Nathan Letwory <nathan@letworyinteractive.com>
2025-06-13 12:36:45 +0300

    Adapt default docker for usage on Linux.
    * don't clone repo into image
    * attach host repo as volume into image
    * install requirements
    * add build task: build docker, build firmware in docker

--- build_jesterKing.sh
+++ build_jesterKing.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-cd /qmk/qmk_firmware
+cd /qmk
 
-git fetch
-git reset --hard origin/jesterking_liatris
-git submodule update --init --recursive
-
+python3 -m pip install -r requirements.txt
 qmk compile -kb splitkb/kyria/rev3 -km jesterKing
-cp *.uf2 /qmk/local/.
+#cp *.uf2 /qmk/local/.
