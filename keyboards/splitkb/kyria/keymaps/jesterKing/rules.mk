8dd98e124b323331bf10212a616e4971d7152e49
Nathan Letwory <nathan@letworyinteractive.com>
2025-07-01 08:35:24 +0300

    beautify rules.mk

--- keyboards/splitkb/kyria/keymaps/jesterKing/rules.mk
+++ keyboards/splitkb/kyria/keymaps/jesterKing/rules.mk
@@ -1,22 +1,22 @@
-OLED_ENABLE           = yes      # Enables the use of OLED displays
-ENCODER_ENABLE        = yes      # Enables the use of one or more encoders
-RGBLIGHT_ENABLE       = no       # Enable keyboard RGB underglow
-LEADER_ENABLE         = yes      # Enable the Leader Key feature
-TAP_DANCE_ENABLE      = yes      # Enable tap dance
-MOUSEKEY_ENABLE       = yes      # Enable mouse keys
-EXTRAKEY_ENABLE   =     yes      # Enable audio and system control keys
-COMBO_ENABLE          = yes      # Enable combos
-CONSOLE_ENABLE        = no       # Enable debug printing, use in concert with hid-listen
-KEYCODE_STRING_ENABLE = yes      # Enable readable logging> get_keycode_string(kc)
-RGB_MATRIX_ENABLE     = no       # Disable keyboard RGB matrix, as it is enabled by default on rev3
+OLED_ENABLE            = yes      # Enables the use of OLED displays
+ENCODER_ENABLE         = yes      # Enables the use of one or more encoders
+RGBLIGHT_ENABLE        = no       # Enable keyboard RGB underglow
+LEADER_ENABLE          = yes      # Enable the Leader Key feature
+TAP_DANCE_ENABLE       = yes      # Enable tap dance
+MOUSEKEY_ENABLE        = yes      # Enable mouse keys
+EXTRAKEY_ENABLE        = yes      # Enable audio and system control keys
+COMBO_ENABLE           = yes      # Enable combos
+CONSOLE_ENABLE         = no       # Enable debug printing, use in concert with hid-listen
+KEYCODE_STRING_ENABLE  = yes      # Enable readable logging> get_keycode_string(kc)
+RGB_MATRIX_ENABLE      = no       # Disable keyboard RGB matrix, as it is enabled by default on rev3
 
-LTO_ENABLE            = yes      # Enable link-time optimization for smaller firmware
-STENO_ENABLE          = yes      # Uses 1664 bytes
-UNICODEMAP_ENABLE     = yes      # Enable unicode map, use with X(i)
+LTO_ENABLE             = yes      # Enable link-time optimization for smaller firmware
+STENO_ENABLE           = yes      # Uses 1664 bytes
+UNICODEMAP_ENABLE      = yes      # Enable unicode map, use with X(i)
 
 POINTING_DEVICE_ENABLE = no
 #POINTING_DEVICE_DRIVER = pimoroni_trackball
 
-WPM_ENABLE = yes
+WPM_ENABLE             = yes
 
-CONVERT_TO=liatris
+CONVERT_TO             = liatris
